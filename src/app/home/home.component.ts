2242616ebca3c1836676fa3bed769aac0fabae50
sansr <sansr@SANSR-ZCZEZ.partnet.cisco.com>
2019-11-26 16:05:15 +0530

    Adding images and font styles for home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,22 +11,23 @@ export class HomeComponent implements OnInit {
     {
       name : "Search",
       content : "Do you want to know more about Shivamogga!?",
-      src : "https://storage.googleapis.com/twg-content/images/MarketingResources_Thumbnail_Search.width-1200.jpg",
+      src : "https://cdn.pixabay.com/photo/2016/08/03/13/56/vector-1566737_960_720.png",
       color: true
     }, {
       name : "Food",
       content : "Do you want to have Shivamogga special food delivered to your door step!?",
-      src : "https://www.archanaskitchen.com/images/archanaskitchen/1-Author/themadscientistskitchen-gmail.com/Spongy_Quinoa_Idli_for_a_Healthy_Breakfast-2.jpg",
-      color: false
+      src : "https://images.indianexpress.com/2018/04/order-food-online.jpg?w=759",
+      color: true
     }, {
       name : "Shopping",
       content : "Do you want buy something from Shivamogga!?",
-      src : "https://img.freepik.com/free-photo/shopping-trolley-with-packets_23-2147948107.jpg?size=338&ext=jpg",
+      src : "https://www.intheblack.com/-/media/intheblack/allimages/magazine-2018/11-november/small-shopping-bags.jpg?rev=2ad3af44e054440598b93c6c3c442aa1",
       color: true
     }, {
       name : "Travel",
       content : "Do you want to travel to the most exiting places near Shivamogga!?",
-      src : "https://cdn.pixabay.com/photo/2019/02/28/04/54/car-4025379_960_720.png",
+      //src : "https://cdn.pixabay.com/photo/2018/05/21/18/48/clipart-3419075_960_720.png",
+      src : "https://cdn.pixabay.com/photo/2016/06/20/07/02/bus-1468141_960_720.png",
       color: true
     }
   ];
